415ad6eef91c9c6cf3b9cefe2bf0deb5ab6c1e18
petermnhull <petermnhull@gmail.com>
2022-01-18 19:42:27 +0000

    Switches response codes to use HTTP status constants

--- sraniq/views/index.py
+++ sraniq/views/index.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, asdict
-
+from http import HTTPStatus
 from sanic.request import Request
 from sanic.views import HTTPMethodView
 from sanic.response import HTTPResponse, json
@@ -14,7 +14,7 @@ class IndexViewGet:
 class IndexView(HTTPMethodView):
     def get(self, request: Request) -> HTTPResponse:
         is_alive = request.app.ctx.health()
-        code = 200 if is_alive else 500
+        code = HTTPStatus.OK if is_alive else HTTPStatus.INTERNAL_SERVER_ERROR
         data = IndexViewGet(
             request.app.name,
             is_alive,
